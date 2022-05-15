eb900c38656a50e007ba3c311d0d4ed8ba4250f8
Egemen <egemenwasd@gmail.com>
2022-05-15 17:22:29 +0300

    initial commit
    kitten model, MVC, CRUD, routes for all 7 RESTful actions are created

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:notice] = 'Succesfully created!'
+      redirect_to root_url
+    else
+      flash[:notice] = 'Something went wrong, try again!'
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      flash[:notice] = 'Succesfully updated!'
+      redirect_to root_url
+    else
+      flash[:notice] = 'Something went wrong, try again!'
+    end
+  end
+
+	def destroy
+		@kitten = Kitten.find(params[:id])
+		@kitten.destroy
+		flash[:notice] = 'Deleted!'
+		redirect_to root_url
+	end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:id, :name, :age, :softness, :cuteness)
+  end
+end
